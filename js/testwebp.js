6038edbd009da0d30aeec307cab27147599c0b5b
MBpro <65220848+MBpro@users.noreply.github.com>
2020-11-08 17:21:23 +0100

    fixed script

---
+++ js/testwebp.js
@@ -0,0 +1,8 @@
+testWebP(function (support) {
+
+    if (support == true) {
+        document.querySelector('body').classList.add('webp');
+    } else {
+        document.querySelector('body').classList.add('no-webp');
+    }
+});
