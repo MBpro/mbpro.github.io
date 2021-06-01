db570146949b271b347d2b3911fe126638a749b4
mbpro <651717@gmail.com>
2021-06-01 18:55:03 +0200

    added file dvygunok

--- js/testwebp.js
+++
@@ -1,2 +0,0 @@
-
-function testWebP(e) { var s = new Image; s.onload = s.onerror = function () { e(2 == s.height) }, s.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA" } testWebP((function (e) { 0 != e || document.querySelector("body").classList.remove("webp") }));
