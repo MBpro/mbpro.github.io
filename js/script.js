2aaa40bbf372b64e6c05fed75a89fce673d917ef
mbpro <651717@gmail.com>
2021-06-06 22:08:21 +0200

    Added js function for scroll

--- js/script.js
+++ js/script.js
@@ -10116,17 +10116,20 @@ questionForm();
 const scrollToTopBtn = document.querySelector('#scroll-to-top-btn');
 const body = document.querySelector('body');
 
-window.addEventListener('scroll',function(){
+function showButton(){
   if (window.pageYOffset > body.clientWidth) { //если значение прокрутки больше чем высота экрана то удаляем класс hidden в которм в css установлено display="none"
     scrollToTopBtn.classList.remove('hidden');
   }else if(window.pageYOffset < body.clientWidth && !scrollToTopBtn.classList.contains('hidden')){ //если значение прокрутки меньше чем высота экрана и отсутствует класс hidden то добавляем класс hidden в которм в css установлено display="none"
     scrollToTopBtn.classList.add('hidden');
   }
-})
+}
 
-scrollToTopBtn.addEventListener('click', function(){
+function scrollToTop(){
   window.scrollTo(0, 0);
-});
+}
+
+window.addEventListener('scroll',showButton);
+scrollToTopBtn.addEventListener('click',scrollToTop); 
 
 
 ;
